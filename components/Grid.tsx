746af38e864069cb0e43520e77d7ddc28e5e70c5
Yann BRONNER <yann.bronner@gmail.com>
2024-05-23 00:21:41 +0200

    have a straight line is better !

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 
+
 const Cell: React.FC<{ borderBottom?: boolean, selectColor: string }> = ({ borderBottom, selectColor }) => {
   const [clicked, setClicked] = useState<boolean>(false);
   const [assignedColor, setAssignedColor] = useState<string>("");
@@ -21,9 +22,9 @@ const Cell: React.FC<{ borderBottom?: boolean, selectColor: string }> = ({ borde
   return (
     <div
       onClick={handleOnClick}
-      className={`h-6 w-6 border box-border flex items-center justify-center ${isABorderColor(assignedColor) && clicked ? "border-4" : ""} ${clicked ? `${assignedColor} rounded-full` : ""}`}
+      className={`relative h-6 w-6 border box-border flex items-center justify-center ${isABorderColor(assignedColor) && clicked ? "border-4" : ""} ${clicked ? `${assignedColor} rounded-full` : ""}`}
     >
-      {borderBottom && <div className="box-content bg-black min-h-[2px] w-full"></div>}
+      {borderBottom && <div className="absolute box-content bg-black min-h-[2px] w-6"></div>}
     </div>
   );
 };
