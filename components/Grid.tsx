5f2171aeb0127c5621729e31431f70acaa5d271a
Yann BRONNER <yann.bronner@gmail.com>
2024-05-23 00:05:33 +0200

    fix empty note

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,33 +1,35 @@
-import { useState } from "react"
+
+import { useState } from "react";
 
 const Cell: React.FC<{ borderBottom?: boolean, selectColor: string }> = ({ borderBottom, selectColor }) => {
-  const [clicked, setClicked] = useState<boolean>(false)
-  const [assignedColor, setAssignedColor] = useState<string>("")
+  const [clicked, setClicked] = useState<boolean>(false);
+  const [assignedColor, setAssignedColor] = useState<string>("");
 
   const isABorderColor = (assignedColor: string) => {
-    if (assignedColor === "") return false
-    return assignedColor.startsWith("border-")
-  }
+    if (assignedColor === "") return false;
+    return assignedColor.startsWith("border-");
+  };
 
   const handleOnClick = () => {
     if (!selectColor) {
-      return
+      return;
     }
-    setClicked(!clicked)
-    setAssignedColor(selectColor)
-  }
+    setClicked(!clicked);
+    setAssignedColor(selectColor);
+  };
 
   return (
     <div
       onClick={handleOnClick}
-      className={`h-6 w-6 border flex items-center justify-center ${isABorderColor(assignedColor) ? "border-4" : ""} ${clicked ? `${assignedColor} rounded-full` : ""}`}>
-      {borderBottom && <div className="bg-black min-h-[2px] w-full"></div>}
+      className={`h-6 w-6 border box-border flex items-center justify-center ${isABorderColor(assignedColor) && clicked ? "border-4" : ""} ${clicked ? `${assignedColor} rounded-full` : ""}`}
+    >
+      {borderBottom && <div className="box-content bg-black min-h-[2px] w-full"></div>}
     </div>
-  )
-}
+  );
+};
 
 export const Grid: React.FC<{ selectedColor: string }> = ({ selectedColor }) => {
-  const notes = new Array(1500).fill(0)
+  const notes = new Array(1500).fill(0);
   return (
     <div className="flex justify-center">
       <div className="grid grid-rows-15 grid-flow-col">
@@ -35,13 +37,13 @@ export const Grid: React.FC<{ selectedColor: string }> = ({ selectedColor }) => 
           if (i % 15 === 3 || i % 15 === 5 || i % 15 === 7 || i % 15 === 9 || i % 15 === 11) {
             return (
               <Cell key={i} borderBottom={true} selectColor={selectedColor} />
-            )
+            );
           }
           return (
             <Cell key={i} borderBottom={false} selectColor={selectedColor} />
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
